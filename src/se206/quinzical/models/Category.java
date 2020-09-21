5d92967b6295471cd08aab709a0b85328f605613
Matt Moran <15150339+DarkMatterMatt@users.noreply.github.com>
2020-09-22 00:04:12 +1200

    Category calculates question value, returns active/next question

--- src/se206/quinzical/models/Category.java
+++ src/se206/quinzical/models/Category.java
@@ -1,50 +1,77 @@
 package se206.quinzical.models;
+
 import java.util.ArrayList;
 import java.util.List;
 
-import javafx.beans.Observable;
-import javafx.beans.property.BooleanProperty;
-import javafx.beans.property.SimpleBooleanProperty;
-import javafx.util.Callback;
-
 public class Category {
-	String _name;
-	List<Question> _questions = new ArrayList<Question>();
 	boolean _isSelected = false;
-	
-	public String getName() {
-		return _name;
-	}
-	
-	public void setSelected() {
-		_isSelected = true;
-	}
-	
-	public void setUnselected() {
-		_isSelected = false;
-	}
-	public boolean isSelected() {
-		return _isSelected;
-	}
-	
+	String _name;
+	List<Question> _questions = new ArrayList<>();
+	private int _activeQuestion = 0;
+
 	public Category(List<String> questions, String name) {
 		_name = name;
-		for (String question: questions) {
+		for (String question : questions) {
 			try {
 				Question q = new Question(question);
 				_questions.add(q);
-			}catch(IllegalArgumentException e) {
+			} catch (IllegalArgumentException e) {
 				//
 			}
-
 		}
 	}
-	
-	
+
 	public Category(List<Question> questions, String name, boolean placeHolder) {
 		_questions = questions;
 		_name = name;
 	}
-	
-	
+
+	public Question getActiveQuestion() {
+		if (_questions.size() > _activeQuestion) {
+			return _questions.get(_activeQuestion);
+		}
+		return null;
+	}
+
+	public int getActiveValue() {
+		return 100 * (_activeQuestion + 1);
+	}
+
+	public String getName() {
+		return _name;
+	}
+
+	/**
+	 * Returns the number of questions that have not been attempted
+	 */
+	public long getNumRemaining() {
+		return _questions.stream().filter(q -> q.getStatus() == Question.Status.UNATTEMPTED).count();
+	}
+
+	/**
+	 * Returns the number of questions that have been attempted
+	 */
+	public long getNumAttempted() {
+		return _questions.stream().filter(q -> q.getStatus() != Question.Status.UNATTEMPTED).count();
+	}
+
+	public boolean isSelected() {
+		return _isSelected;
+	}
+
+	public void moveToNextQuestion() {
+		_activeQuestion++;
+	}
+
+	public void reset() {
+		_activeQuestion = 0;
+	}
+
+	public void setSelected() {
+		_isSelected = true;
+	}
+
+	public void setUnselected() {
+		_isSelected = false;
+	}
 }
