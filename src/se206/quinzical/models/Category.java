ba78b3d83cc66191e72955a3351ccac010a3f984
Hajin <68993476+hkim532@users.noreply.github.com>
2020-09-21 16:36:21 +1200

    Update Category.java
    added methods that check if the category is selected

--- src/se206/quinzical/models/Category.java
+++ src/se206/quinzical/models/Category.java
@@ -2,13 +2,29 @@ package se206.quinzical.models;
 import java.util.ArrayList;
 import java.util.List;
 
+import javafx.beans.Observable;
+import javafx.beans.property.BooleanProperty;
+import javafx.beans.property.SimpleBooleanProperty;
+import javafx.util.Callback;
+
 public class Category {
 	String _name;
 	List<Question> _questions = new ArrayList<Question>();
-	
+	boolean _isSelected = false;
 	
 	public String getName() {
 		return _name;
+	}
+	
+	public void setSelected() {
+		_isSelected = true;
+	}
+	
+	public void setUnselected() {
+		_isSelected = false;
+	}
+	public boolean isSelected() {
+		return _isSelected;
 	}
 	
 	public Category(List<String> questions, String name) {
