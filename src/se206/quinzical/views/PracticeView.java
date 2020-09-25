8840a7f3093d2df3f3c5a451dccc343917eaef9b
hajin <68993476+hkim532@users.noreply.github.com>
2020-09-25 18:59:47 +1200

    Practice Module updated

--- src/se206/quinzical/views/PracticeView.java
+++ src/se206/quinzical/views/PracticeView.java
@@ -10,10 +10,68 @@ public class PracticeView extends View {
 
 	public PracticeView(QuinzicalModel model) {
 		_model = model;
+		
+		CategoriesListView list = new CategoriesListView(model);
+		SwitcherView content = new PracticeSwitcher(model);
+		
+		_container.getChildren().addAll(list.getView(), content.getView());
+		
 	}
 
 	@Override
 	public HBox getView() {
 		return _container;
 	}
+}
+
+class PracticeSwitcher extends SwitcherView{
+	
+	private final QuinzicalModel _model;
+	
+	// incorrect, correct, question asking, 
+	private final AnswerView _answerQuestion;
+	private final CorrectView _correctPane;
+	private final IncorrectView _incorrectPane;
+	private final HBox _nothingChosen;
+	
+	public PracticeSwitcher(QuinzicalModel model) {
+		_model = model;
+
+		//initialise possible views
+		_answerQuestion = new AnswerView(_model.getPracticeModel());
+		_correctPane = new CorrectView(_model.getPracticeModel());
+		_incorrectPane = new IncorrectView(_model.getPracticeModel());
+		_nothingChosen = new HBox();
+		
+		
+		getView().getChildren().addAll(_answerQuestion.getView(), _correctPane.getView(), _incorrectPane.getView(), _nothingChosen);
+		
+		//start with nothing chosen.
+		switchToView(_nothingChosen);
+		_model.getPracticeModel().getStateProperty().addListener((obs, old, val) -> onModelStateChange());
+		
+	}
+
+	private void onModelStateChange() {
+		System.out.println("reached here model state change");
+		switch(_model.getPracticeModel().getState()) {
+			case SELECT_CATEGORY:
+				
+				switchToView(_nothingChosen);
+				break;
+			case INCORRECT_ANSWER:
+				switchToView(_incorrectPane.getView());
+				break;
+			case CORRECT_ANSWER:
+				switchToView(_correctPane.getView());
+				break;
+			case ANSWER_QUESTION:
+				switchToView(_answerQuestion.getView());
+				break;
+			default:
+				throw new UnsupportedOperationException("Unexpected model state");
+		}
+	}
+	
+	
 }
