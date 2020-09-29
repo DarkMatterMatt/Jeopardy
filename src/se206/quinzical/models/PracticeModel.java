10bddddb68fcab46ad66bb378fe756ed385843f2
Matt Moran <15150339+DarkMatterMatt@users.noreply.github.com>
2020-09-30 11:06:51 +1300

    Fix NPE on load from save file

--- src/se206/quinzical/models/PracticeModel.java
+++ src/se206/quinzical/models/PracticeModel.java
@@ -1,10 +1,14 @@
 package se206.quinzical.models;
 
+import java.util.Collections;
 import java.util.List;
 
 public class PracticeModel extends QuizModel {
+	private final List<Category> _categories;
+
 	public PracticeModel(QuinzicalModel model) {
 		super(model);
+		_categories = _model.getCategories();
 	}
 
 	@Override
@@ -13,31 +17,28 @@ public class PracticeModel extends QuizModel {
 			throw new IllegalStateException("Previous state should be ANSWER_QUESTION, found " + getState());
 		}
 		boolean correct = getCurrentQuestion().checkAnswer(answer);
-		
-		// count number of attempts 
+
+		// count number of attempts
 		Question q = _model.getPracticeModel().getCurrentQuestion();
 		// increase number of attempt for that question
 		if(!correct) q.setNumAttempted(q.getNumAttempted()+1);
-		
+
 		// refresh (needed for hint to appear at third time)
 		setState(State.SELECT_CATEGORY);
 		setState(State.ANSWER_QUESTION);
-		
+
 		// if that question has been answered 3 times, reset that question
 		// and change the active question to different random question
 		if(correct || q.getNumAttempted()>=3) {
 			q.setNumAttempted(0);
-			q.getCategory().setActiveQUestionInPracticeModule(q.getCategory().getRandomQuestion());			
+			q.getCategory().setActiveQUestionInPracticeModule(q.getCategory().getRandomQuestion());
 			setState(correct ? State.CORRECT_ANSWER : State.INCORRECT_ANSWER);
 		}
-		
-
-		
 	}
 
 	@Override
 	public List<Category> getCategories() {
-		return _model.getCategories();
+		return Collections.unmodifiableList(_categories);
 	}
 
 	@Override
