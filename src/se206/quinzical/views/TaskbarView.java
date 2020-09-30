138ec9b8434fa56f31d7525c9e70a98056700d5d
Matt Moran <15150339+DarkMatterMatt@users.noreply.github.com>
2020-10-01 12:58:06 +1300

    Add speed slider to taskbar

--- src/se206/quinzical/views/TaskbarView.java
+++ src/se206/quinzical/views/TaskbarView.java
@@ -1,12 +1,16 @@
 package se206.quinzical.views;
 
+import javafx.animation.KeyFrame;
+import javafx.animation.Timeline;
 import javafx.application.Platform;
+import javafx.scene.control.Slider;
 import javafx.scene.control.Tooltip;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.Pane;
 import javafx.scene.layout.StackPane;
+import javafx.util.Duration;
 import se206.quinzical.models.QuinzicalModel;
 
 /**
@@ -17,6 +21,7 @@ public class TaskbarView extends View {
 	private final QuinzicalModel _model;
 	private final ImageView _reset;
 	private final StackPane _toggleText;
+	private final Timeline _speedSliderHideTimer = new Timeline();
 
 	public TaskbarView(QuinzicalModel model) {
 		_model = model;
@@ -33,7 +38,7 @@ public class TaskbarView extends View {
 
 		// home button view
 		ImageView home = createButton("../assets/home.png");
-		home.setOnMouseClicked(e->{
+		home.setOnMouseClicked(e -> {
 			//change state to menu
 			model.backToMainMenu();
 		});
@@ -44,7 +49,7 @@ public class TaskbarView extends View {
 		HBox noText = new HBox(createButton("../assets/notext.png"));
 
 		SwitcherView s = new SwitcherView() {};
-		s.getView().getChildren().addAll(text,noText);
+		s.getView().getChildren().addAll(text, noText);
 
 		// enable text
 		_toggleText = s.getView();
@@ -55,10 +60,13 @@ public class TaskbarView extends View {
 		Tooltip.install(text, new Tooltip("Text Currently Visible"));
 		Tooltip.install(noText, new Tooltip("Text Currently Invisible"));
 
-		_container.getChildren().addAll(_toggleText, home, _reset, exit);
+		// text speed slider
+		Slider speedSlider = createSpeedSlider();
+
+		_container.getChildren().addAll(speedSlider, _toggleText, home, _reset, exit);
 		_container.setSpacing(10);
 		_container.getStyleClass().add("taskbar");
-        addStylesheet("taskbar.css");
+		addStylesheet("taskbar.css");
 
 		// show reset button ONLY during game state
 		onModelStateChange();
@@ -76,6 +84,53 @@ public class TaskbarView extends View {
 		return v;
 	}
 
+	private Slider createSpeedSlider() {
+		Slider slider = new Slider(-1, 1, 0);
+		Tooltip tooltip = new Tooltip();
+		Tooltip.install(slider, tooltip);
+
+		_speedSliderHideTimer.setOnFinished(ev -> {
+			tooltip.hide();
+			_model.save();
+		});
+
+		slider.valueProperty().addListener((obs, old, num) -> {
+			double val = num.doubleValue();
+			if (val != 0 && Math.abs(val) < 0.1) {
+				// snap to zero
+				slider.setValue(0);
+				return;
+			}
+			// change value so it is 0.5x speed on the left, 1x speed in the middle, 2x speed on the right
+			double speed = 0.25 * val * val + 0.75 * val + 1;
+			_model.getTextToSpeech().setSpeedMultiplier(speed);
+
+			// update tooltip, 600ms display duration
+			tooltip.setText("Speech speed: " + Math.round(speed * 100) / 100.0 + "x");
+			if (tooltip.getOwnerWindow() != null) {
+				if (!tooltip.isShowing()) {
+					tooltip.show(tooltip.getOwnerWindow());
+				}
+				_speedSliderHideTimer.getKeyFrames().setAll(new KeyFrame(Duration.millis(600)));
+				_speedSliderHideTimer.playFromStart();
+			}
+		});
+
+		// not-excellent way of reversing our quadratic transformation
+		double savedSpeedMultiplier = _model.getTextToSpeech().getSpeedMultiplier();
+		double savedSpeed = -1;
+		while (0.25 * savedSpeed * savedSpeed + 0.75 * savedSpeed + 1 < savedSpeedMultiplier) {
+			savedSpeed += 0.01;
+		}
+		slider.setValue(savedSpeed);
+
+		return slider;
+	}
+
+	public Pane getView() {
+		return _container;
+	}
+
 	private void onModelStateChange() {
 		// show reset button ONLY during game state
 		switch (_model.getState()) {
@@ -91,8 +146,5 @@ public class TaskbarView extends View {
 			default:
 				throw new UnsupportedOperationException("Unexpected model state");
 		}
-	}
-	public Pane getView() {
-		return _container;
 	}
 }
