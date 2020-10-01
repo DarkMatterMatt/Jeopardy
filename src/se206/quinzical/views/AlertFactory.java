450039d72a27acaeaefb4093cc9caa567c26d391
hajin <68993476+hkim532@users.noreply.github.com>
2020-10-01 17:09:06 +1300

    Delegated alert job to AlertFactory, added reset alert

---
+++ src/se206/quinzical/views/AlertFactory.java
@@ -0,0 +1,56 @@
+package se206.quinzical.views;
+
+import javafx.application.Platform;
+import javafx.scene.control.Alert;
+import javafx.scene.control.ButtonBar;
+import javafx.scene.control.ButtonType;
+import javafx.scene.control.DialogPane;
+import javafx.scene.control.Alert.AlertType;
+import javafx.stage.Stage;
+import javafx.stage.StageStyle;
+import se206.quinzical.DragAndResizeHelper;
+import se206.quinzical.models.QuinzicalModel;
+
+public class AlertFactory {
+	
+	public static void getExitAlert(Taskbar origin) {
+		Alert exitAlert = new Alert(AlertType.NONE);
+		exitAlert.setHeaderText("Sad to see you go!");
+		exitAlert.setContentText("Are you sure you want to exit Quinzical?");
+		
+		// style alert
+		DialogPane dialogue = exitAlert.getDialogPane();
+		ButtonType yes = new ButtonType("Yup",ButtonBar.ButtonData.YES);
+		ButtonType no = new ButtonType("Actually nah",ButtonBar.ButtonData.NO);
+		dialogue.getButtonTypes().addAll(yes, no);
+		dialogue.getStylesheets().add(origin.getClass().getResource("../styles/dialogue.css").toExternalForm());
+		// stage of dialogue
+		Stage diaStage = (Stage) dialogue.getScene().getWindow();
+		diaStage.initStyle(StageStyle.UNDECORATED);
+		DragAndResizeHelper.addResizeListener(diaStage);
+		exitAlert.showAndWait().filter(res -> res==yes).ifPresent(res -> {
+			Platform.exit();
+		});
+	}
+
+	public static void getResetAlert(Taskbar origin, QuinzicalModel model) {
+		Alert resetAlert = new Alert(AlertType.NONE);
+		resetAlert.setHeaderText("Be careful! You are about to lose your money");
+		resetAlert.setContentText("Are you sure you want to reset?");
+		
+		// style alert
+		DialogPane dialogue = resetAlert.getDialogPane();
+		ButtonType yes = new ButtonType("Yup",ButtonBar.ButtonData.YES);
+		ButtonType no = new ButtonType("Actually nah",ButtonBar.ButtonData.NO);
+		dialogue.getButtonTypes().addAll(yes, no);
+		dialogue.getStylesheets().add(origin.getClass().getResource("../styles/dialogue.css").toExternalForm());
+		// stage of dialogue
+		Stage diaStage = (Stage) dialogue.getScene().getWindow();
+		diaStage.initStyle(StageStyle.UNDECORATED);
+		DragAndResizeHelper.addResizeListener(diaStage);
+		resetAlert.showAndWait().filter(res -> res==yes).ifPresent(res -> {
+			model.reset();
+		});
+	}
+
+}
--- src/se206/quinzical/views/Taskbar.java
+++ src/se206/quinzical/views/Taskbar.java
@@ -40,35 +40,14 @@ public class Taskbar extends ViewBase {
 		// exit button view
 		ImageView exit = createButton("../assets/exit.png");
 		exit.setOnMouseClicked(e -> {
-			Alert exitAlert = new Alert(AlertType.NONE);
-			exitAlert.setHeaderText("Sad to see you go!");
-			exitAlert.setContentText("Are you sure you want to exit Quinzical?");
-			
-			// style alert
-			DialogPane dialogue = exitAlert.getDialogPane();
-			ButtonType yes = new ButtonType("Yup",ButtonBar.ButtonData.YES);
-			ButtonType no = new ButtonType("Actually nah",ButtonBar.ButtonData.NO);
-			dialogue.getButtonTypes().addAll(yes, no);
-			dialogue.getStylesheets().add(getClass().getResource("../styles/dialogue.css").toExternalForm());
-			// stage of dialogue
-			Stage diaStage = (Stage) dialogue.getScene().getWindow();
-			diaStage.initStyle(StageStyle.UNDECORATED);
-			DragAndResizeHelper.addResizeListener(diaStage);
-			
-			exitAlert.showAndWait().filter(res -> res==yes).ifPresent(res -> {
-				Platform.exit();
-			});
-
-
+			AlertFactory.getExitAlert(this);
 		});
 		Tooltip.install(exit, new Tooltip("Quit"));
 
 		// reset button view
 		_reset = createButton("../assets/reset.png");
 		_reset.setOnMouseClicked(e -> {
-			
-			model.reset();
-			
+			AlertFactory.getResetAlert(this, model);
 		});
 		Tooltip.install(_reset, new Tooltip("Reset game"));
 
