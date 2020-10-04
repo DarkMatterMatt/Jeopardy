f9a0b221726346b492724638f8efef2b78c19975
Matt Moran <15150339+DarkMatterMatt@users.noreply.github.com>
2020-10-04 13:31:05 +1300

    Make PracticeSwitcher an inner class of PracticeSwitch

--- src/se206/quinzical/views/PracticeSwitch.java
+++ src/se206/quinzical/views/PracticeSwitch.java
@@ -24,7 +24,7 @@ public class PracticeSwitch extends ViewBase {
 
 		CategoriesList list = new CategoriesList(_model.getPracticeModel());
 		SwitcherBase content = new PracticeSwitcher(_model.getPracticeModel());
-		//center the answer view
+		// center the answer view
 		VBox listContainer = new VBox(list.getView());
 		HBox.setHgrow(content.getView(), Priority.ALWAYS);
 		listContainer.getStyleClass().add("practice");
@@ -37,59 +37,59 @@ public class PracticeSwitch extends ViewBase {
 	public HBox getView() {
 		return _container;
 	}
-}
 
-class PracticeSwitcher extends SwitcherBase {
-	// incorrect, correct, question asking
-	private final AnswerPane _answerPane;
-	private final CorrectPane _correctPane;
-	private final IncorrectPane _incorrectPane;
-	private final HBox _nothingChosen;
-	private final PracticeModel _practiceModel;
+	private static class PracticeSwitcher extends SwitcherBase {
+		// incorrect, correct, question asking
+		private final AnswerPane _answerPane;
+		private final CorrectPane _correctPane;
+		private final IncorrectPane _incorrectPane;
+		private final HBox _nothingChosen;
+		private final PracticeModel _practiceModel;
 
-	public PracticeSwitcher(PracticeModel practiceModel) {
-		_practiceModel = practiceModel;
+		public PracticeSwitcher(PracticeModel practiceModel) {
+			_practiceModel = practiceModel;
 
-		// initialise possible views
-		_answerPane = new AnswerPane(_practiceModel);
-		_correctPane = new CorrectPane(_practiceModel);
-		_incorrectPane = new IncorrectPane(_practiceModel);
-		_nothingChosen = new HBox();
+			// initialise possible views
+			_answerPane = new AnswerPane(_practiceModel);
+			_correctPane = new CorrectPane(_practiceModel);
+			_incorrectPane = new IncorrectPane(_practiceModel);
+			_nothingChosen = new HBox();
 
-		getView().getChildren().addAll(_answerPane.getView(), _correctPane.getView(), _incorrectPane.getView(), _nothingChosen);
+			getView().getChildren().addAll(_answerPane.getView(), _correctPane.getView(), _incorrectPane.getView(), _nothingChosen);
 
-		// add state change listener
-		onModelStateChange();
-		_practiceModel.getStateProperty().addListener((obs, old, val) -> onModelStateChange());
-	}
+			// add state change listener
+			onModelStateChange();
+			_practiceModel.getStateProperty().addListener((obs, old, val) -> onModelStateChange());
+		}
 
-	private void onModelStateChange() {
-		switch (_practiceModel.getState()) {
-			case SELECT_CATEGORY:
-				switchToView(_nothingChosen);
-				Question q = _practiceModel.getCurrentQuestion();
-				if (q != null) {
-					_practiceModel.selectCategory(q.getCategory());
-				}
-				break;
-			case INCORRECT_ANSWER:
-				switchToView(_incorrectPane.getView());
-				break;
-			case CORRECT_ANSWER:
-				switchToView(_correctPane.getView());
-				break;
-			case RETRY_INCORRECT_ANSWER:
-				_answerPane.flashAnswerIncorrect(ev -> {
-					_answerPane.clearInput();
-					_answerPane.focusInput();
-					_answerPane.setHintVisible(_practiceModel.getCurrentQuestion().getNumAttempted() >= 2);
-				});
-				break;
-			case ANSWER_QUESTION:
-				switchToView(_answerPane.getView());
-				break;
-			default:
-				throw new UnsupportedOperationException("Unexpected model state");
+		private void onModelStateChange() {
+			switch (_practiceModel.getState()) {
+				case SELECT_CATEGORY:
+					switchToView(_nothingChosen);
+					Question q = _practiceModel.getCurrentQuestion();
+					if (q != null) {
+						_practiceModel.selectCategory(q.getCategory());
+					}
+					break;
+				case INCORRECT_ANSWER:
+					switchToView(_incorrectPane.getView());
+					break;
+				case CORRECT_ANSWER:
+					switchToView(_correctPane.getView());
+					break;
+				case RETRY_INCORRECT_ANSWER:
+					_answerPane.flashAnswerIncorrect(ev -> {
+						_answerPane.clearInput();
+						_answerPane.focusInput();
+						_answerPane.setHintVisible(_practiceModel.getCurrentQuestion().getNumAttempted() >= 2);
+					});
+					break;
+				case ANSWER_QUESTION:
+					switchToView(_answerPane.getView());
+					break;
+				default:
+					throw new UnsupportedOperationException("Unexpected model state");
+			}
 		}
 	}
 }
