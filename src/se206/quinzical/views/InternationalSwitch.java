996d62031c77ec56c9ab1d5e28b6a5a8c246b594
Hajin <68993476+hkim532@users.noreply.github.com>
2020-10-15 23:18:13 +1300

    added answerpane and livespane

--- src/se206/quinzical/views/InternationalSwitch.java
+++ src/se206/quinzical/views/InternationalSwitch.java
@@ -1,12 +1,39 @@
 package se206.quinzical.views;
 
+import javafx.geometry.Pos;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.Priority;
 import se206.quinzical.models.QuinzicalModel;
 
 public class InternationalSwitch extends SwitcherBase {
+	private final HBox _container = new HBox();
+	private final QuinzicalModel _model;
+	private final AnswerPane _answerPane;
+	private final LivesPane _livesPane;
 
-	public InternationalSwitch(QuinzicalModel _model) {
+	public InternationalSwitch(QuinzicalModel model) {
+		_model = model;
+		// make a question box
+		_answerPane = new AnswerPane(_model.getPracticeModel());
 		
+		// make lives pane
+		_livesPane = new LivesPane(_model);
+
+		HBox.setHgrow(_answerPane.getView(), Priority.ALWAYS);
+
+		// add it to container
+		_container.getChildren().addAll(_answerPane.getView(), _livesPane.getView());
+
+		// if the game changes to other modes, make sure to empty out the international question selection
+		_model.getStateProperty().addListener((obs, newV, oldV) -> onModelStateChange());
 		
+		getView().getChildren().addAll(_container);
+		getView().setAlignment(Pos.CENTER);
 	}
+
+	private void onModelStateChange() {
+
+	}
+
 
 }
