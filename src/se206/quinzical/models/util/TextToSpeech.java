caf73ed724a2b0ddb17a750c61c4192891c0d945
Matt Moran <15150339+DarkMatterMatt@users.noreply.github.com>
2020-10-18 17:40:55 +1300

    Cancel TTS when new TTS starts
    Bug: changing too quickly results in the first one not being cancelled properly

--- src/se206/quinzical/models/util/TextToSpeech.java
+++ src/se206/quinzical/models/util/TextToSpeech.java
@@ -3,11 +3,14 @@ package se206.quinzical.models.util;
 import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.OutputStreamWriter;
+import java.util.concurrent.atomic.AtomicReference;
+import java.util.function.Consumer;
 
 /**
  * Class that deals with text to speech stuff using espeak and festival
  */
 public class TextToSpeech {
+	private transient final AtomicReference<Process> _activeTTS = new AtomicReference<>();
 	private double _speedMultiplier = 1;
 
 	private Process executeInBackground(String... cmd) {
@@ -33,7 +36,11 @@ public class TextToSpeech {
 	 * @param words words to speak
 	 */
 	public void speak(String words) {
-		speakFestival(words);
+		speak(words, null);
+	}
+
+	public void speak(String words, Consumer<Process> callback) {
+		speakFestival(words, callback);
 	}
 
 	/**
@@ -52,24 +59,52 @@ public class TextToSpeech {
 	 * @param words words to speak
 	 */
 	public void speakFestival(String words) {
-		Process p = executeInBackground("festival");
-		if (p != null) {
-			try (BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(p.getOutputStream()))) {
-				// select custom NZ voice
-				bw.write("(voice_akl_nz_jdt_diphone)");
-				bw.newLine();
+		speakFestival(words, null);
+	}
 
-				// set voice speed
-				bw.write(String.format("(Parameter.set 'Duration_Stretch %.2f)", 1 / getSpeedMultiplier()));
-				bw.newLine();
-
-				// speak!
-				bw.write("(SayText \"" + words.replace("\"", "") + "\")");
-				bw.newLine();
-			}
-			catch (IOException e) {
-				e.printStackTrace();
-			}
+	public void speakFestival(String words, Consumer<Process> callback) {
+		Process p;
+		try {
+			p = new ProcessBuilder("festival").start();
 		}
+		catch (IOException e) {
+			return;
+		}
+
+		// kill any existing speaking
+		Process oldP = _activeTTS.getAndSet(p);
+		if (oldP != null) {
+			oldP.descendants().filter(ProcessHandle::isAlive).forEach(ProcessHandle::destroy);
+		}
+
+		try (BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(p.getOutputStream()))) {
+			// select custom NZ voice
+			bw.write("(voice_akl_nz_jdt_diphone)");
+			bw.newLine();
+
+			// set voice speed
+			bw.write(String.format("(Parameter.set 'Duration_Stretch %.2f)", 1 / getSpeedMultiplier()));
+			bw.newLine();
+
+			// speak!
+			bw.write("(SayText \"" + words.replace("\"", "") + "\")");
+			bw.newLine();
+
+			// exit after finished speaking
+			bw.write("(quit)");
+			bw.newLine();
+		}
+		catch (IOException e) {
+			e.printStackTrace();
+		}
+
+		p.onExit().thenAccept(p2 -> {
+			_activeTTS.compareAndSet(p, null);
+
+			// execute callback
+			if (callback != null) {
+				callback.accept(p);
+			}
+		});
 	}
 }
