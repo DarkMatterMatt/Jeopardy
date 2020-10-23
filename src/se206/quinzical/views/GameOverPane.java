435882c695081945efa1f6f2598d7e9533a6415e
Matt Moran <15150339+DarkMatterMatt@users.noreply.github.com>
2020-10-23 23:08:53 +1300

    Add input to submit name to leaderboard

--- src/se206/quinzical/views/GameOverPane.java
+++ src/se206/quinzical/views/GameOverPane.java
@@ -16,18 +16,23 @@ public class GameOverPane extends ViewBase {
 	private final PresetQuinzicalModel _model;
 	private final Label _subtitleLabel = new Label();
 	private final Label _titleLabel = new Label();
+	private final AnswerTextField _nameInputView;
 
 	public GameOverPane(PresetQuinzicalModel model) {
 		_model = model;
+		_nameInputView = new AnswerTextField(_model, name -> {
+			_model.getModel().getLeaderboardModel().addScore(_model.getScore(), name);
+			_model.getModel().showLeaderboard();
+			_model.reset();
+		});
 
 		_titleLabel.getStyleClass().addAll("text-bold", "text-gold", "title");
 		_subtitleLabel.getStyleClass().addAll("text-white", "subtitle");
 
-		Label resetLabel = new Label("Reset");
-		resetLabel.getStyleClass().addAll("btn", "text-black", "text-bold", "reset");
-		resetLabel.setOnMouseClicked(ev -> _model.reset());
+		Label submitNameLabel = new Label("Submit your name to the leaderboard");
+		submitNameLabel.getStyleClass().addAll("submit-name", "text-small", "text-white");
 
-		_container.getChildren().addAll(_titleLabel, _subtitleLabel, resetLabel);
+		_container.getChildren().addAll(_titleLabel, _subtitleLabel, _nameInputView.getView(), submitNameLabel);
 		_container.getStyleClass().add("game-over");
 		addStylesheet("game-over.css");
 
@@ -55,7 +60,6 @@ public class GameOverPane extends ViewBase {
 			_titleLabel.setText("Welp.");
 			_subtitleLabel.setText("I guess there's a first time for everything.\nYou managed to get every question wrong!");
 		}
-
-		_model.getModel().getLeaderboardModel().addScore(_model.getScore(), "TEMPORARY_NAME");
+		_nameInputView.focus();
 	}
 }
