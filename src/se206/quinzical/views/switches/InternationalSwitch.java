df624f460654ec926814c72466397a905ec9feb0
hajin <68993476+hkim532@users.noreply.github.com>
2020-10-29 22:42:25 +1300

    javadoc & autoformat

--- src/se206/quinzical/views/switches/InternationalSwitch.java
+++ src/se206/quinzical/views/switches/InternationalSwitch.java
@@ -12,74 +12,77 @@ import se206.quinzical.views.pane.AnswerPane;
 import se206.quinzical.views.pane.answerstate.CorrectPane;
 import se206.quinzical.views.pane.answerstate.IncorrectPane;
 
+/**
+ * InternationalSwitch is a Switch type
+ * Represents international mode in which the user can aim
+ * for the highest score with 3 given lives
+ * It switches between (answerpane & livespane), incorrect answer pane, correct answer pane
+ */
 public class InternationalSwitch extends SwitcherBase {
-	private final HBox _container = new HBox();
-	private final QuinzicalModel _model;
-	private final VBox _answerPane = new VBox();
-	private final Lives _livesPane;
-	private final IncorrectPane _incorrectPane;
-	private final CorrectPane _correctPane;
+    private final HBox _container = new HBox();
+    private final QuinzicalModel _model;
+    private final VBox _answerPane = new VBox();
+    private final Lives _livesPane;
+    private final IncorrectPane _incorrectPane;
+    private final CorrectPane _correctPane;
 
-	public InternationalSwitch(QuinzicalModel model) {
-		_model = model;
+    public InternationalSwitch(QuinzicalModel model) {
+        _model = model;
 
-		// initialise screens
-		_incorrectPane = new IncorrectPane(model.getPracticeModel());
-		_correctPane = new CorrectPane(model.getPracticeModel());
+        // initialise screens
+        _incorrectPane = new IncorrectPane(model.getPracticeModel());
+        _correctPane = new CorrectPane(model.getPracticeModel());
 
-		// make a question box, make lives pane
-		updateAnswerPane();
-		_livesPane = new Lives(_model);
-		HBox.setHgrow(_answerPane, Priority.ALWAYS);
-		VBox.setVgrow(_answerPane, Priority.ALWAYS);
-		// add them to the container
-		_container.getChildren().addAll(_answerPane, _livesPane.getView());
-		_container.setSpacing(48);
+        // make a question box, make lives pane
+        updateAnswerPane();
+        _livesPane = new Lives(_model);
+        HBox.setHgrow(_answerPane, Priority.ALWAYS);
+        VBox.setVgrow(_answerPane, Priority.ALWAYS);
+        // add them to the container
+        _container.getChildren().addAll(_answerPane, _livesPane.getView());
+        _container.setSpacing(48);
 
-//		// if the game changes to other modes, make sure to empty out the international question selection
-//		_model.getStateProperty().addListener((obs, newV, oldV) -> onModelStateChange());
+        getView().getChildren().addAll(_container, _incorrectPane.getView(), _correctPane.getView());
+        getView().setAlignment(Pos.CENTER);
+        getView().setPadding(new Insets(48));
 
-		getView().getChildren().addAll(_container, _incorrectPane.getView(), _correctPane.getView());
-		getView().setAlignment(Pos.CENTER);
-		getView().setPadding(new Insets(48));
-		
-		_model.getPracticeModel().getStateProperty().addListener((obs, newV, oldV) -> onModelStateChange());
-		onModelStateChange();
+        _model.getPracticeModel().getStateProperty().addListener((obs, newV, oldV) -> onModelStateChange());
+        onModelStateChange();
 
-	}
+    }
 
-	private void updateAnswerPane() {
-		_answerPane.getChildren().clear();
-		VBox answerPane = new AnswerPane(_model.getPracticeModel()).getView();
-		VBox.setVgrow(answerPane, Priority.ALWAYS);
-		_answerPane.getChildren().add(answerPane);
-	}
+    private void updateAnswerPane() {
+        _answerPane.getChildren().clear();
+        VBox answerPane = new AnswerPane(_model.getPracticeModel()).getView();
+        VBox.setVgrow(answerPane, Priority.ALWAYS);
+        _answerPane.getChildren().add(answerPane);
+    }
 
-	private void onModelStateChange() {
-		switch (_model.getPracticeModel().getState()) {
-		case SELECT_CATEGORY:
-		case ANSWER_QUESTION:
-			updateAnswerPane();
-			switchToView(_container);
-			break;
-		case INCORRECT_ANSWER:
-			switchToView(_incorrectPane.getView());
-			// change the question
-			_model.getInternationalCategory()
-					.setActiveQuestionInPracticeModule(_model.getInternationalCategory().getRandomQuestion());
+    private void onModelStateChange() {
+        switch (_model.getPracticeModel().getState()) {
+            case SELECT_CATEGORY:
+            case ANSWER_QUESTION:
+                updateAnswerPane();
+                switchToView(_container);
+                break;
+            case INCORRECT_ANSWER:
+                switchToView(_incorrectPane.getView());
+                // change the question
+                _model.getInternationalCategory()
+                        .setActiveQuestionInPracticeModule(_model.getInternationalCategory().getRandomQuestion());
 
-			break;
-		case CORRECT_ANSWER:
-			switchToView(_correctPane.getView());
-			// change the question
-			_model.getInternationalCategory()
-					.setActiveQuestionInPracticeModule(_model.getInternationalCategory().getRandomQuestion());
+                break;
+            case CORRECT_ANSWER:
+                switchToView(_correctPane.getView());
+                // change the question
+                _model.getInternationalCategory()
+                        .setActiveQuestionInPracticeModule(_model.getInternationalCategory().getRandomQuestion());
 
-			break;
-		default:
-			break;
-		}
-	}
+                break;
+            default:
+                break;
+        }
+    }
 
 
 }
